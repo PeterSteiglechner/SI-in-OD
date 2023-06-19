5aec783f03feda7503bb3e0e445b48e533cb110a
Peter Steiglechner <35730409+PeterSteiglechner@users.noreply.github.com>
2023-06-19 15:36:50 +0200

    Update run.sh
    rename parameters

--- run.sh
+++ run.sh
@@ -6,18 +6,19 @@ conda activate phd1
 
 export n=$1
 export k=$2
-export h=$3
-export delta=$4
-export kappa=$5
-export commf=$6
-export sig=$7
-export p_rewire=$8
-export T=$9
-export resolution=${10}
-export seed=${11}
+export kin=$3
+export kout=$4
+export delta=$5
+export kappa=$6
+export commf=$7
+export sig=$8
+export p_rewire=$9
+export T=${10}
+export resolution=${11}
+export seed=${12}
 
-echo "$n $k $h $delta $kappa $commf $sig $p_rewire $T $resolution $seed"
+echo "$n $k $kin $kout $delta $kappa $commf $sig $p_rewire $T $resolution $seed"
 
-python3 batch_simulations.py $n $k $h $delta $kappa $commf $sig $p_rewire $T $resolution $seed 
+python3 batch_simulations.py $n $k $kin $kout $delta $kappa $commf $sig $p_rewire $T $resolution $seed 
 
 echo "finished"
